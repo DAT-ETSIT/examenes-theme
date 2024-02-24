057a0e555a0852e6f6dac277805d0c2206fa8809
Pablofl01 <pablofl01@gmail.com>
2024-02-24 16:49:47 +0100

    Added search

--- script.js
+++ script.js
@@ -24,6 +24,8 @@ function fixTable() {
   const rows = Array.from(table.querySelectorAll('tr'));
   rows.forEach((row) => {
     const fileColumn = row.children[0];
+    fileColumn.classList.add("indexcolname");
+
     const iconColumn = document.createElement('td');
     iconColumn.className = 'img-wrap';
     iconColumn.style = "width: 1%";
@@ -82,7 +84,51 @@ function addTitle() {
   header.insertAdjacentElement('afterend', container);
 }
 
+function addSearch() {
+  const input = document.createElement('input');
+  input.type = 'search';
+  input.id = 'search';
+  input.setAttribute('placeholder', 'Buscar');
+  document.getElementById('page-header').appendChild(input);
+
+  const sortColumns = Array.from(document.querySelectorAll('thead a'));
+  const nameColumns = Array.from(document.querySelectorAll('tbody .indexcolname'));
+  const rows = nameColumns.map(({ parentNode }) => parentNode);
+  const fileNames = nameColumns.map(({ textContent }) => textContent);
+
+  function filter(value) {
+    // Allow tabbing out of the search input and skipping the sort links
+    // when there is a search value.
+    sortColumns.forEach((link) => {
+      if (value) {
+        link.tabIndex = -1;
+      } else {
+        link.removeAttribute('tabIndex');
+      }
+    });
+
+    // Test the input against the file/folder name.
+    let even = false;
+    fileNames.forEach((name, i) => {
+      if (!value || name.toLowerCase().includes(value.toLowerCase())) {
+        const className = even ? 'even' : '';
+        rows[i].className = className;
+        even = !even;
+      } else {
+        rows[i].className = 'hidden';
+      }
+    });
+  }
+
+  document.getElementById('search').addEventListener('input', ({ target }) => {
+    filter(target.value);
+  });
+
+  filter('');
+}
+
 document.addEventListener("DOMContentLoaded", function (event) {
   fixTable();
   addTitle();
+  addSearch();
 });
